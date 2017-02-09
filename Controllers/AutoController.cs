dba25437fdb8fa03106d212eefb54707c11bd55f
Karim Samiul <Karim Samiul>
2017-02-08 19:23:44 -0500

    Check in

--- Controllers/AutoController.cs
+++ Controllers/AutoController.cs
@@ -8,13 +8,17 @@ public class AutoController : Controller
     public IActionResult Index()
 
     {
-        return Ok("This is the index of the AutoController");
+        //return Ok("This is the index of the AutoController");
+
+        return View();
     }
 
     public IActionResult Quote()
     {   
         
-        return Ok("This is the quote of the AutoController");
+        //return Ok("This is the quote of the AutoController");
+
+        return View();
 
     }
 
