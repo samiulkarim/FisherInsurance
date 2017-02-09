575b0527492b70c4025f4aa1b48c177d50602136
Karim Samiul <Karim Samiul>
2017-02-08 19:10:46 -0500

    Check in

--- Controllers/AutoController.cs
+++ Controllers/AutoController.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+using FisherInsurance.Models;
+using System;
 
 public class AutoController : Controller
 
@@ -12,10 +14,17 @@ public class AutoController : Controller
     }
 
     public IActionResult Quote()
-    {
+    {   
+        Quote quote = new Quote
+{
+ Id = 345,
+ Product = "Auto Insurance",
+ ExpireDate = DateTime.Now.AddDays(45),
+ Price = 45.00M
+ };
         //return Ok("This is the quote of the AutoController");
 
-        return View();
+        return View(quote);
     }
 
 }
